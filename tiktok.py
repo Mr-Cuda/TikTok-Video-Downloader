5791905585d6458e9835648e6f1757a411b21e68
Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-06-06 09:00:31 +0200

    Create tiktok.py

---
+++ tiktok.py
@@ -0,0 +1,19 @@
+class color:
+   DARKCYAN = '\033[36m'
+    
+import requests
+import tikvid
+from colorama import init
+
+video = input('URL: ')
+id = tikvid.parseLink(video)
+
+output = tikvid.downloadLink(id)
+v_content = requests.get(output)
+
+with open('TikVideo.mp4', 'wb') as f:
+	f.write(v_content.content)
+  
+print(f' ')
+print(color.DARKCYAN + f'Video downloaded!')
+print(f' ')
